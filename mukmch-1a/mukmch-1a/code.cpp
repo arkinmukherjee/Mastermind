314f1922c96f9639147652bfa71ce00ffec876ec
Blake McHale <mchale.b@husky.neu.edu>
2019-09-13 17:37:04 -0400

    uncomment

--- mukmch-1a/mukmch-1a/code.cpp
+++ mukmch-1a/mukmch-1a/code.cpp
@@ -26,62 +26,62 @@ void code::initRandom() {
 	}
 }
 
-//const int code::checkCorrect(code &guess) { //TODO find out if const can be used
-//	const vector<int> guessCode = guess.getCode();
-//	int counterCorrect = 0;
-//
-//	if (checkSize(guessCode.size())) {
-//		for (int i = 0; i < guessCode.size(); i++){
-//			if (secretCode[i] == guessCode[i]) {
-//				counterCorrect++;
-//			}
-//		}
-//	}
-//	return counterCorrect;
-//}
-//
-//const int code::checkIncorrect(code &guess) { //TODO find out if const can be used
-//	const vector<int> guessCode = guess.getCode();
-//
-//	if (!checkSize(guessCode.size())) {
-//		return -1;
-//	}
-//
-//	int counterIncorrect = 0;
-//	vector<bool> incorrectFlags;
-//	incorrectFlags.resize(secretCode.size(), false);
-//
-//	for (int i = 0; i < guessCode.size(); i++) {
-//		for (int j = 0; j < guessCode.size(); i++) {
-//			if (secretCode[i] == guessCode[j] && !incorrectFlags[i]) {
-//				counterIncorrect++;
-//				incorrectFlags[i] = true;
-//				break;
-//			}
-//		}
-//	}
-//
-//	return counterIncorrect;
-//	
-//}
-//
-//const bool code::checkSize(const int &guessSize) {
-//	if (guessSize == secretCode.size()) {
-//		return true;
-//	}
-//	else {
-//		return false;
-//	}
-//}
-//
-//const bool code::checkRange(const int &val1) {
-//	if (val1 < maxValue){
-//		return true;
-//	}
-//	else{
-//		return false;
-//	}
-//}
+const int code::checkCorrect(code &guess) { //TODO find out if const can be used
+	const vector<int> guessCode = guess.getCode();
+	int counterCorrect = 0;
+
+	if (checkSize(guessCode.size())) {
+		for (int i = 0; i < guessCode.size(); i++){
+			if (secretCode[i] == guessCode[i]) {
+				counterCorrect++;
+			}
+		}
+	}
+	return counterCorrect;
+}
+
+const int code::checkIncorrect(code &guess) { //TODO find out if const can be used
+	const vector<int> guessCode = guess.getCode();
+
+	if (!checkSize(guessCode.size())) {
+		return -1;
+	}
+
+	int counterIncorrect = 0;
+	vector<bool> incorrectFlags;
+	incorrectFlags.resize(secretCode.size(), false);
+
+	for (int i = 0; i < guessCode.size(); i++) {
+		for (int j = 0; j < guessCode.size(); i++) {
+			if (secretCode[i] == guessCode[j] && !incorrectFlags[i]) {
+				counterIncorrect++;
+				incorrectFlags[i] = true;
+				break;
+			}
+		}
+	}
+
+	return counterIncorrect;
+	
+}
+
+const bool code::checkSize(const int &guessSize) {
+	if (guessSize == secretCode.size()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+const bool code::checkRange(const int &val1) {
+	if (val1 < maxValue){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 
 const vector<int> code::getCode() const {
 	return secretCode;
