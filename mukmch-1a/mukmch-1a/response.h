d3fe752dc67672f1bae6eaacb59944eb2f8c167a
Blake McHale <mchale.b@husky.neu.edu>
2019-09-20 16:48:36 -0400

    Added response class

--- mukmch-1a/mukmch-1a/response.h
+++ mukmch-1a/mukmch-1a/response.h
@@ -2,3 +2,23 @@
 // Author: Blake McHale and Arkin Mukherjee
 // Date: 9/20/2019
 // Project 1b
+
+#include "code.h"
+
+class response
+// Establishes class for storing response values from guess codes
+{
+	public:
+		response(code& secret, code& guess);
+		void setCorrect() { numCorrect = secret.checkCorrect(guess); };
+		void setIncorrect() { numIncorrect = secret.checkIncorrect(guess); };
+		int getCorrect() { return numIncorrect; };
+		int getIncorrect() { return numCorrect; };
+
+	private:
+		int numCorrect;
+		int numIncorrect;
+		code secret;
+		code guess;
+
+};
